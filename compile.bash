1777a0164e4e51368850c36cdc9e8e889e31ca24
pavel bolhar <gfijrb@yandex.ua>
2022-06-22 16:46:09 +0300

    add Multicall2.sol for 0.8 version

--- compile.bash
+++ compile.bash
@@ -1,3 +1,4 @@
 solc -o ./build/solc0.5/ --bin --optimize --overwrite --asm-json --abi --metadata ./src/solc0.5/Multicall.sol
 solc -o ./build/solc0.5/ --bin --optimize --overwrite --asm-json --abi --metadata ./src/solc0.5/Multicall2.sol
+solc -o ./build/solc0.8/ --bin --optimize --overwrite --asm-json --abi --metadata ./src/solc0.8/Multicall2.sol
 solc -o ./build/solc0.8/ --bin --optimize --overwrite --asm-json --abi --metadata ./src/solc0.8/MulticallTestnet.sol
